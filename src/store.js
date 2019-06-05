0a7e41cbef04addec0fa87c817ab0912f369e7f2
Duke <35853481+hudngo@users.noreply.github.com>
2019-06-04 20:03:17 -0700

    moar imgs
    nah

--- src/store.js
+++ src/store.js
@@ -1,4 +1,9 @@
-import opersTracker_Logo from "./Components/Layouts/Images/tennis_mainImg.png";
+import tennismainImg from "./Components/Layouts/Images/tennis_mainImg.png";
+import wellnessmainImg from "./Components/Layouts/Images/wellness_mainImg.png";
+import activmainImg from "./Components/Layouts/Images/activity_mainImg.png";
+import dancemainImg from "./Components/Layouts/Images/dance_mainImg.png";
+import gymmainImg from "./Components/Layouts/Images/gym_mainImg.png";
+import martialmainImg from "./Components/Layouts/Images/martial_mainImg.png";
 
 export const areasBuildings = [
   "open",
@@ -10,64 +15,78 @@ export const areasBuildings = [
 export const exercises = [
   {
     id: "activities-room",
-    img: opersTracker_Logo,
+    img: activmainImg,
     // img: "https://uploads.codesandbox.io/uploads/user/21e62c85-25b9-4ac0-a30e-e3170524188d/wzSU-opersTracker_Logo.png",
-    title: "Activities Room",
+    title: "Activity Room",
     description: "Delts exercise...",
     areasBuildings: "east gym"
   },
   {
     id: "dance-studio",
-    img: opersTracker_Logo,
+    img: dancemainImg,
     title: "Dance Studio",
     description: "Delts exercise...",
     areasBuildings: "east gym"
   },
   {
+    id: "gym-room",
+    img: gymmainImg,
+    title: "Gymasium",
+    description: "Delts exercise...",
+    areasBuildings: "east gym"
+  },
+  {
+    id: "martialarts-room",
+    img: martialmainImg,
+    title: "Martial Arts Room",
+    description: "Delts exercise...",
+    areasBuildings: "east gym"
+  },
+  {
     id: "basketball-courts",
-    img: opersTracker_Logo,
+    img: tennismainImg,
     title: "Basketball Courts",
     description: "Triceps exercise...",
     areasBuildings: "sports areas"
   },
   {
     id: "swimming-pool",
-    img: opersTracker_Logo,
+    img: tennismainImg,
     title: "Swimming Pool",
     description: "Biceps exercise...",
     areasBuildings: "sports areas"
   },
   {
     id: "tennis-courts",
-    img: opersTracker_Logo,
+    img: tennismainImg,
     title: "Tennis Courts",
     description: "Biceps exercise...",
     areasBuildings: "sports areas"
   },
   {
     id: "front-desk",
-    img: opersTracker_Logo,
+    img: tennismainImg,
     title: "Front Desk",
     description: "Chest exercise...",
     areasBuildings: "opers admin"
   },
   {
     id: "locker-room",
-    img: opersTracker_Logo,
+    img: tennismainImg,
     title: "Locker Room",
     description: "Chest exercise...",
     areasBuildings: "opers admin"
   },
   {
     id: "wellness-center",
-    img: opersTracker_Logo,
+    img: wellnessmainImg,
     title: "Wellness Center",
     description: "Back and leg exercise...",
     areasBuildings: "wellness center"
   },
   {
     id: "multi-purpose-room",
-    img: opersTracker_Logo,
+    img: tennismainImg,
     title: "Multi-Purpose Room",
     description: "Back and biceps exercise...",
     areasBuildings: "multi-purpose room"

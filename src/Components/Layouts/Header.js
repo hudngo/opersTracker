e7cba74bb9cfac8803933a0415bed39843ce3dec
Duke <35853481+hudngo@users.noreply.github.com>
2019-06-05 16:47:45 -0700

    search and updated dependencies
    nah

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,16 +1,186 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "material-ui";
 import logo from "./Images/opersTracker_Logo.png";
+// import IconButton from '@material-ui/core/IconButton';
+// import InputBase from '@material-ui/core/InputBase';
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: 100,
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit"
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: 120,
+      "&:focus": {
+        width: 200
+      }
+    }
+  }
+}));
+
+// const styles = theme => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     flexGrow: 1,
+//     display: 'none',
+//     [theme.breakpoints.up('sm')]: {
+//       display: 'block',
+//     },
+//   },
+//   search: {
+//     position: 'relative',
+//     borderRadius: theme.shape.borderRadius,
+//     backgroundColor: fade(theme.palette.common.white, 0.15),
+//     '&:hover': {
+//       backgroundColor: fade(theme.palette.common.white, 0.25),
+//     },
+//     marginLeft: 0,
+//     width: '100%',
+//     [theme.breakpoints.up('sm')]: {
+//       marginLeft: theme.spacing(1),
+//       width: 'auto',
+//     },
+//   },
+//   searchIcon: {
+//     width: theme.spacing(7),
+//     height: '100%',
+//     position: 'absolute',
+//     pointerEvents: 'none',
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   inputRoot: {
+//     color: 'inherit',
+//   },
+//   inputInput: {
+//     padding: theme.spacing(1, 1, 1, 7),
+//     transition: theme.transitions.create('width'),
+//     width: '100%',
+//     [theme.breakpoints.up('sm')]: {
+//       width: 120,
+//       '&:focus': {
+//         width: 200,
+//       },
+//     },
+//   },
+// });
 
 console.log(logo);
+// const classes = useStyles();
 
-export default props => (
-  <AppBar position="static">
-    <Toolbar>
-      <img
-        src="https://uploads.codesandbox.io/uploads/user/21e62c85-25b9-4ac0-a30e-e3170524188d/wzSU-opersTracker_Logo.png"
-        alt="UCSC OPERS"
-      />
-    </Toolbar>
-  </AppBar>
-);
+// export default props => (
+
+//   <AppBar position="static">
+//     <Toolbar>
+//       <img
+//         src="https://uploads.codesandbox.io/uploads/user/21e62c85-25b9-4ac0-a30e-e3170524188d/wzSU-opersTracker_Logo.png"
+//         alt="UCSC OPERS"
+//       />
+//       <div style={styles.search}>
+//             <div className={styles.searchIcon}>
+//               <SearchIcon />
+//             </div>
+//             <InputBase
+//               placeholder="Search…"
+//               classes={{
+//                 root: styles.inputRoot,
+//                 input: styles.inputInput,
+//               }}
+//               inputProps={{ 'aria-label': 'Search' }}
+//             />
+//           </div>
+//     </Toolbar>
+//   </AppBar>
+// );
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <img
+            src="https://uploads.codesandbox.io/uploads/user/21e62c85-25b9-4ac0-a30e-e3170524188d/wzSU-opersTracker_Logo.png"
+            alt="UCSC OPERS"
+          />
+          {/* <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Open drawer"
+          >
+            <MenuIcon /> */}
+          {/* </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Material-UI
+          </Typography> */}
+          <div style={{ padding: 20 }} />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ "aria-label": "Search" }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}

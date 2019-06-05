0c57e9dedd0135e58ffc114d603d361ab1ac43e1
Duke <35853481+hudngo@users.noreply.github.com>
2019-06-04 19:33:07 -0700

    layout + Imgs
    nah

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from "react";
 import { Grid, Paper, Typography, List, Button } from "material-ui";
 import { ListItem, ListItemText } from "material-ui/List";
-import { typography } from "material-ui/styles";
+import mainImage from "../Layouts/Images/mainImg.png";
 
 const styles = theme => ({
   Paper: {
     padding: 20,
     marginTop: 10,
     marginBottom: 10,
-    height: 600,
+    height: 200,
     overflowY: "auto"
   },
   Paper1: {
@@ -28,17 +28,27 @@ const styles = theme => ({
   // },
   banner: {
     padding: 60,
-    marginTop: 20
+    marginTop: 20,
+    height: 400
   },
   Paper2: {
-    padding: 20,
+    padding: 0,
     marginTop: 10,
     marginBottom: 10,
-    height: 200,
+    height: 600,
     overflowY: "auto"
+  },
+  root: {
+    flexGrow: 1
+  },
+  // paper: {
+  //   height: 140,
+  //   width: 100,
+  // },
+  control: {
+    padding: theme.spacing(2)
   }
 });
-
 export default ({
   exercises,
   atheltics,
@@ -46,23 +56,32 @@ export default ({
   onSelect,
   exercise: {
     id,
-    title = "Welcome!",
-    description = "Please select an area or building from the list on the left.",
+    img = mainImage,
+    title = "East Field House — Opers",
+    // img = "https://uploads.codesandbox.io/uploads/user/21e62c85-25b9-4ac0-a30e-e3170524188d/wzSU-opersTracker_Logo.png",
+    description = "Sports, exercise, and recreation complex",
     hours = ""
   }
 }) => (
-  <Grid container>
+  <Grid container className={styles.root} spacing={2}>
     {/* <Grid container> */}
     <Grid item xs={3}>
       <Paper style={styles.banner}>
-        <Typography variant="display1" style={{ marginTop: 0, padding: 20 }}>
+        <div style={{ padding: 20 }}>
+          <img src={img} alt="img" />
+        </div>
+        <Typography variant="display1" style={{ marginTop: -30, padding: 20 }}>
           {title}
         </Typography>
+        {/* <div style={{padding: 20}}>
+          <img src={img} alt="img" />
+        </div>
+        <div>{exercises.img}</div> */}
         <Typography
           variant="subheading"
           style={{ marginTop: -20, padding: 20 }}
         >
-          {description} 
+          {description}
         </Typography>
         {/* </Paper> */}
         {/* </Grid>
@@ -92,20 +111,25 @@ export default ({
       </Paper>
       {/* </Grid> */}
     </Grid>
-    <Grid item xs={8}>
+    <Grid item sm={9}>
       <Paper style={styles.Paper2}>
         <Typography variant="display1">{title}</Typography>
         <Typography variant="subheading" style={{ marginTop: 20 }}>
-          {description} <br></br>
-          {hours} <br></br>
-          <Button target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScbvLtTiX53tSWpuVJhdyzwHbQuoX-J6n4Qiad2agzEZXUy_w/viewform">reserve room</Button>
+          {description} <br />
+          {hours} <br />
+          <Button
+            target="_blank"
+            href="https://docs.google.com/forms/d/e/1FAIpQLScbvLtTiX53tSWpuVJhdyzwHbQuoX-J6n4Qiad2agzEZXUy_w/viewform"
+          >
+            reserve room
+          </Button>
         </Typography>
       </Paper>
     </Grid>
 
     <Grid item xs={4}>
       <Paper>
-      {/* {atheltics.map(([group, atheltics]) =>
+        {/* {atheltics.map(([group, atheltics]) =>
             !category || category === group ? (
               <Fragment key={group}>
                 <Typography
@@ -125,19 +149,12 @@ export default ({
             ) : null
           )} */}
       </Paper>
-
     </Grid>
     <Grid item xs={4}>
-      <Paper>
-        this is working
-      </Paper>
-
+      <Paper>this is working</Paper>
     </Grid>
     <Grid item xs={4}>
-      <Paper>
-        this is working
-      </Paper>
-
+      <Paper>this is working</Paper>
     </Grid>
   </Grid>
 );
--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import opersTracker_Logo from "./Components/Layouts/Images/tennis_mainImg.png";
+
 export const areasBuildings = [
   "open",
   "east gym",
@@ -8,63 +10,69 @@ export const areasBuildings = [
 export const exercises = [
   {
     id: "activities-room",
+    img: opersTracker_Logo,
+    // img: "https://uploads.codesandbox.io/uploads/user/21e62c85-25b9-4ac0-a30e-e3170524188d/wzSU-opersTracker_Logo.png",
     title: "Activities Room",
     description: "Delts exercise...",
     areasBuildings: "east gym"
   },
   {
     id: "dance-studio",
+    img: opersTracker_Logo,
     title: "Dance Studio",
     description: "Delts exercise...",
     areasBuildings: "east gym"
   },
   {
     id: "basketball-courts",
+    img: opersTracker_Logo,
     title: "Basketball Courts",
     description: "Triceps exercise...",
     areasBuildings: "sports areas"
   },
   {
     id: "swimming-pool",
+    img: opersTracker_Logo,
     title: "Swimming Pool",
-    description: "the current population is 30",
-    areasBuildings: "sports areas",
-    hours: "Monday-Friday 8:30am - 1:30pm, 3:30pm- 7:30pm"
+    description: "Biceps exercise...",
+    areasBuildings: "sports areas"
   },
   {
     id: "tennis-courts",
+    img: opersTracker_Logo,
     title: "Tennis Courts",
     description: "Biceps exercise...",
     areasBuildings: "sports areas"
   },
   {
     id: "front-desk",
+    img: opersTracker_Logo,
     title: "Front Desk",
     description: "Chest exercise...",
     areasBuildings: "opers admin"
   },
   {
     id: "locker-room",
+    img: opersTracker_Logo,
     title: "Locker Room",
     description: "Chest exercise...",
     areasBuildings: "opers admin"
   },
   {
     id: "wellness-center",
+    img: opersTracker_Logo,
     title: "Wellness Center",
-    description: "the current population is 80",
-    areasBuildings: "wellness center",
-    hours: "Monday-Friday 6:30am - 11pm"
-
+    description: "Back and leg exercise...",
+    areasBuildings: "wellness center"
   },
   {
     id: "multi-purpose-room",
+    img: opersTracker_Logo,
     title: "Multi-Purpose Room",
     description: "Back and biceps exercise...",
     areasBuildings: "multi-purpose room"
   }
 ];
-
 
 export const atheltics = [
   {
@@ -74,7 +82,6 @@ export const atheltics = [
   },
   {
     id: 1,
-    name: "robin",
-    
+    name: "robin"
   }
 ];
